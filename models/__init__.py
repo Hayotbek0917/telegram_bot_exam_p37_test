d9ecfc8487779c476a6487e71b9b8b9ce6569f35
Hayotbek0917 <qaxxorjonovxayotbek@gmail.com>
2026-03-03 11:25:18 +0500

    Initial commit

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+from models.adverts import Advert, AdvertImage, Category, Region, District
+from models.base import Model, Base, CreatedBaseModel
+from models.courses import Course, UserCourse
+from models.users import User
+from models.films import Film
+
